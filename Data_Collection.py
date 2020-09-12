866d3f83bc51a0b759b66ddfbe2bedccd27b924f
Junkyscientist <smartsavior@hotmail.com>
2020-09-12 17:29:20 +0400

    Uploading data and Data script

--- Data_Collection.py
+++ Data_Collection.py
@@ -13,6 +13,6 @@ path = "C:/Users/MGilSoriano/Documents/ds_salary_proj/chromedriver"
 
 df = gs.get_jobs('data scientist', 500, False, path, 15)
 
-df.to_csv('glassdoor_jobs'.csv, index= False)
+df.to_csv('glassdoor_jobs.csv', index = False)
 
 
---
+++ data_cleaning.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 16:21:45 2020
+
+@author:JunkysScientist
+"""
+import pandas as pd
+
+df = pd.read_csv('glassdoor_jobs.csv')
+
